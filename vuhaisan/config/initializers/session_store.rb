aaeb621796925893bacd8deca91a082f576d56a2
Dat Do <datdvt@mobilus.co.jp>
2014-04-30 11:31:04 +0900

    Save cookie at server

--- vuhaisan/config/initializers/session_store.rb
+++ vuhaisan/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Vuhaisan::Application.config.session_store :cookie_store, key: '_vuhaisan_session'
+Vuhaisan::Application.config.session_store :mongoid_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
